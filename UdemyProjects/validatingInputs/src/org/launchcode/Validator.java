cc81e7ea9c43a7fdc521a4b8c2105370c159f47b
yetidie <zombieturpentine@gmail.com>
2019-03-02 20:08:16 -0600

    Work in progres son validating inputs exercise

---
+++ UdemyProjects/validatingInputs/src/org/launchcode/Validator.java
@@ -0,0 +1,20 @@
+package org.launchcode;
+
+
+public class Validator {
+    public int getIntegerBetween(int lower, int upper) {
+//        do {
+//
+//        } while(lower !7);
+        return 0;
+    }
+
+    public int getTensInteger(int lower, int upper) {
+        // TO-DO
+        return 0;
+    }
+}
+
+
+
+
