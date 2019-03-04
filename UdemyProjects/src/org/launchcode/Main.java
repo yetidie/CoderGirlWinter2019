55c93012f3e346e6eddb187efa1921dd2d0ab35f
yetidie <zombieturpentine@gmail.com>
2019-03-03 21:12:32 -0600

    first draft of guessing game.

--- UdemyProjects/src/org/launchcode/Main.java
+++ UdemyProjects/src/org/launchcode/Main.java
@@ -1,33 +1,20 @@
 package org.launchcode;
 import java.util.Scanner;
-import java.util.Random;
+//import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner ( System.in );
-
-        System.out.println ("Want to play a game?");
-        System.out.println ("I'm thinking of a number between 1 and 100. What is it?");
-
-        int guess = 0;
-        System.out.println ("Guess:" + guess);
+        guess g = new guess ();
 
 
-        System.out.println (" Oops, That number isn't between 1 and 100. Try again.");
-
-        System.out.println ("My number is higher.");
-
-        System.out.println ("My number is lower");
+        System.out.println ( "Want to play a game?" );
+        g.guessingGame ( 1, 100 );
+        g.playAgain ();
 
 
-        int answer = 0;
-        System.out.println ("You got it! My number was " + answer);
-
-
-        System.out.println ("Play again?");
-
-
-	// write your code here
     }
 }
+
+
--- UdemyProjects/src/org/launchcode/guess.java
+++ UdemyProjects/src/org/launchcode/guess.java
@@ -1,50 +1,60 @@
 package org.launchcode;
+
 import java.util.Scanner;
 //import java.util.Random;
 
 public class guess {
 
-    int answer = 100;
-    public int guessingGame(){
+
+    public void guessingGame(int lower, int upper) {
         Scanner keyboard = new Scanner ( System.in );
-        int number = keyboard.nextInt (  );
+
         int answer = 33;
-        do{
 
-            System.out.println ("Guess" + keyboard.nextInt ());
-            if(number > answer){
 
-                System.out.println ("My number is lower.");
+        int number;
+        System.out.println ( "I'm thinking of a number between 1 and 100. What is it?" );
+        do {
+            number = keyboard.nextInt ();
+            if (number > 100) {
+                System.out.println ( "Oops! That number isn't between 1 and 100. Try again." );
+            } else if (number > answer) {
 
-            }else if (number < answer) {
-                System.out.println ("My number is higher.");
-            }else if (number = answer) {
-                System.out.println ("Oops! That number isn't between 1 and 100. Try again.");
-            } else {
-                System.out.println ("You got it! My number is " + answer + ".");
+                System.out.println ( "My number is lower." );
+
+            } else if (number < answer) {
+                System.out.println ( "My number is higher." );
+            } else if (number == answer) {
+                System.out.println ( "You got it! My number is " + answer + "." );
+                playAgain ();
             }
-        }while(number != answer);
+
+            System.out.println ( "Guess: " + number );
+
+        } while (number != answer);
+
+
+
     }
 
-    public String playAgain(){
-        Scanner yon = new Scanner(System.in);
 
-        System.out.println ("Play again?");
-        String end = yon.nextLine ().toLowerCase ();
+    public void playAgain() {
+        Scanner keyboard = new Scanner ( System.in );
 
-        if ("yes") {
-            System.out.println ("I'm thinking of a number between 1 and 100. What is it?");
+        System.out.println ( "Play again?" );
+        String end = keyboard.nextLine ().toLowerCase ();
 
-        }else if("no"){
-            System.out.println ("Another time then!");
-        }else{
-            System.out.println ("Error: invalid input. Try again.");
+
+        if (end.equals ( "yes" )) {
+
+            guessingGame ( 1, 100 );
+
+        } else if (end.equals ( "no" )) System.out.println ( "Another time then!" );
+        else {
+            System.out.println ( "Error: invalid input. Try again." );
+            playAgain ();
         }
 
-
-
     }
-
-
-
 }
+
